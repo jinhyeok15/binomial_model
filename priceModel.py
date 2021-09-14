692f7be12f40a0f21b4452a992a307dda19befcf
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-14 22:22:24 +0900

    put option payoff with rnp

--- priceModel.py
+++ priceModel.py
@@ -27,4 +27,17 @@ if __name__ == '__main__':
     p1 = model_price(100, irm[1][0], delta=0.5)
     p2 = model_price(100, irm[1][1], delta=0.5)
     pm = market_data["Price"][1]
-    print(rnp(p1, p2, irm[0], pm, delta=0.5))
+    rnp = rnp(p1, p2, irm[0], pm, delta=0.5)
+    print("rnp: "+str(rnp))
+
+    # put option payoff (RNP approach)
+    r0 = irm[0]
+    r1 = irm[1][0]
+    r2 = irm[1][1]
+    rk = 0.02
+    put_p1 = 100*max(rk-r1, 0)
+    put_p2 = 100*max(rk-r2, 0)
+    exp_value = put_p1*rnp+put_p2*(1-rnp)
+    print("expected value at 6-month: "+str(exp_value))
+    exp_value = model_price(exp_value, r0, delta=0.5)
+    print("discounted value: "+str(exp_value))
