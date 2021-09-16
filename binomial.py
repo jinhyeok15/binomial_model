691d14309281d86eed7cc149e911bc7c27f69223
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-16 20:38:53 +0900

    binomial 최종

--- binomial.py
+++ binomial.py
@@ -2,6 +2,8 @@ class Binomial:
     def __init__(self, size):
         self.size = size
         self.address = []
+        self.tree = {}
+        # make address model
         cnt = 0
         for i in range(size):
             length = 2**size-1
@@ -25,14 +27,52 @@ class Binomial:
                     n += 1
 
     def __str__(self):
+        if self.tree == {}:
+            tree = 'Not append data'
+        else:
+            tree = self.tree
         return f'Binomial Object {self.__class__}\n' + \
                f'Size: {self.size}\n' + \
                f'Address List: {str(self.address)}\n' + \
-               f'Address Length: {len(self.address)}'
+               f'Address Length: {len(self.address)}\n' + \
+               f'Tree Form: {tree}'
 
-    # def append(self, ):
+    def append(self, data):
+        if len(data) != self.size:
+            raise MemoryError
+        cnt = 0
+        for arr in data:
+            if cnt == 0:
+                self.tree[arr] = self.address[cnt]
+                cnt += 1
+                continue
+            for i in arr:
+                self.tree[i] = self.address[cnt]
+                cnt += 1
+        return 1
+
+    def path(self, node, index):
+        if index >= 2**node:
+            raise IndexError
+        address_idx = 2**node-1+index
+        return self.address[address_idx]
+
+    def value(self, node, index):
+        address = self.path(node, index)
+        for key, value in self.tree.items():
+            if address == value:
+                return key
+        return None
 
 
 if __name__ == "__main__":
-    b = Binomial(4)
+    irm = [
+        0.0174,
+        [0.0339, 0.0095]
+    ]
+    b = Binomial(2)
     print(b)
+    b.append(irm)
+    print(str(b.tree))
+    print(b.value(0, 0))
+    print(b.value(1, 0))
