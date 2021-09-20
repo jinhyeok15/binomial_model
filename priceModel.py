3fe0c6d322a4eb650b46fb24019a6835580aecf3
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-20 19:56:51 +0900

    sample main으로 넣기

--- priceModel.py
+++ priceModel.py
@@ -1,21 +1,6 @@
 import numpy as np
 from binomial import Binomial
 
-market_data = {
-    "Maturity": [0.5, 1.0, 1.5],
-    "Price": [99.1338, 97.8925, 96.1531],
-    "Yield": [0.0174, 0.0213, 0.0262],
-    "ForwardRate": [0.0174, 0.0252, 0.0359]
-}
-
-irm = [
-    0.0174,
-    [0.0339, 0.0095]
-]
-
-bi = Binomial(2)
-bi.append(irm)
-
 
 def model_price(value, ir, delta=1):
     return value*np.exp(-ir*delta)
@@ -27,13 +12,28 @@ def rnp(p1, p2, ir, pm, delta=1):
 
 
 if __name__ == '__main__':
+    sample_market_data = {
+        "Maturity": [0.5, 1.0, 1.5],
+        "Price": [99.1338, 97.8925, 96.1531],
+        "Yield": [0.0174, 0.0213, 0.0262],
+        "ForwardRate": [0.0174, 0.0252, 0.0359]
+    }
+
+    sample_irm = [
+        0.0174,
+        [0.0339, 0.0095]
+    ]
+
+    bi = Binomial(2)
+    bi.append(sample_irm)
+
     r0 = bi.value(0, 0)
     r1 = bi.value(1, 0)
     r2 = bi.value(1, 1)
     print(model_price(100, r1, delta=0.5))
     p1 = model_price(100, r1, delta=0.5)
     p2 = model_price(100, r2, delta=0.5)
-    pm = market_data["Price"][1]
+    pm = sample_market_data["Price"][1]
     rnp = rnp(p1, p2, r0, pm, delta=0.5)
     print("rnp: "+str(rnp))
 
