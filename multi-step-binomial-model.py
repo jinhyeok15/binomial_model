1cf0426b30d261a10b566d0033aac25bad8ce6f4
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-24 21:55:08 +0900

    error 수정 최종

--- multi-step-binomial-model.py
+++ multi-step-binomial-model.py
@@ -38,13 +38,11 @@ while i >= 0:
         c2 = rear[2*j+1]
         rij = irm.value(i, j)
         price = model_price(c1, rij, delta=0.5)*not_adjusted_rnp + \
-                model_price(c2, rij, delta=0.5)*(1-not_adjusted_rnp)
+            model_price(c2, rij, delta=0.5)*(1-not_adjusted_rnp)
         node_price.append(price)
     bond_price.insert(0, node_price)
     rear = node_price
     i -= 1
-
-print(bond_price)
 
 bp = Binomial(3)
 bp.append(bond_price)
@@ -53,6 +51,11 @@ bp10 = bp.value(1, 0)
 bp11 = bp.value(1, 1)
 
 adjusted_rnp = rnp(bp10, bp11, r00, pm3, delta=0.5)
-print("adjusted rnp: "+str(adjusted_rnp))
+print("adjusted rnp: " + str(adjusted_rnp))
+
+expected_price = model_price(bp10, r10, delta=0.5)*adjusted_rnp + \
+    model_price(bp11, r11, delta=0.5)*(1-adjusted_rnp)
+print("expected price: " + str(expected_price))
+
 if __name__ == '__main__':
-    print("-------------------")
+    print("-----------------------------------")
