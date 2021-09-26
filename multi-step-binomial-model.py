b615bc0f973d9c9d24129ca3f527419c88adf445
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-26 21:32:20 +0900

    bond_model func 추가

--- multi-step-binomial-model.py
+++ multi-step-binomial-model.py
@@ -29,23 +29,7 @@ if __name__ == '__main__':
     not_adjusted_rnp = rnp(p10, p11, r00, pm2, delta=0.5)
     print("rnp not adjusted: " + str(not_adjusted_rnp))
 
-    i = irm.size - 1  # size = 3
-    rear = [100 for r in range(2 ** irm.size)]
-    bond_price = [rear]
-    while i >= 0:
-        node_price = []
-        for j in range(2 ** i):
-            c1 = rear[2 * j]
-            c2 = rear[2 * j + 1]
-            rij = irm.value(i, j)
-            price = model_price(c1, rij, delta=0.5) * not_adjusted_rnp + \
-                model_price(c2, rij, delta=0.5) * (1 - not_adjusted_rnp)
-            node_price.append(price)
-        bond_price.insert(0, node_price)
-        rear = node_price
-        i -= 1
-
-    bp = bi.model(bond_price)
+    bp = bond_model(100, irm, not_adjusted_rnp)
 
     bp10 = bp.value(1, 0)
     bp11 = bp.value(1, 1)
--- price.py
+++ price.py
@@ -1,4 +1,5 @@
 import numpy as np
+import binomial as bi
 
 
 def model_price(value, ir, delta=1.0):
@@ -13,3 +14,22 @@ def rnp(up, dn, ir, market_price, delta=1.0):
 def simultaneous_equation(coefficient, objective):
     inv_coefficient = np.linalg.inv(coefficient)
     return inv_coefficient*objective
+
+
+def bond_model(principal, irm, rn_prop):
+    i = irm.size - 1
+    rear = [principal for r in range(2 ** irm.size)]
+    bond_price = [rear]
+    while i >= 0:
+        node_price = []
+        for j in range(2 ** i):
+            c1 = rear[2 * j]
+            c2 = rear[2 * j + 1]
+            rij = irm.value(i, j)
+            price = model_price(c1, rij, delta=0.5) * rn_prop + \
+                model_price(c2, rij, delta=0.5) * (1 - rn_prop)
+            node_price.append(price)
+        bond_price.insert(0, node_price)
+        rear = node_price
+        i -= 1
+    return bi.model(bond_price)
